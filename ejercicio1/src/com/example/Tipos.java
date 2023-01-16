c80cc0b9337c7b89e2e233755cc920a6827ff20b
Alvarado Rodríguez <lucy-elena.alvarado-rodriguez@capgemini.com>
2023-01-16 17:10:56 +0100

    ejercicio 1

---
+++ ejercicio1/src/com/example/Tipos.java
@@ -0,0 +1,37 @@
+package com.example;
+
+public class Tipos {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        //enteros
+        byte variable1 = 5;
+        System.out.println(variable1);
+        short variable2 = 10;
+        System.out.println(variable2);
+        int  variable3= 30;
+        System.out.println(variable3);
+        long variable4 = 100;
+        System.out.println(variable4);
+        //decimales
+        float variable5 = 5.5f;
+        System.out.println(variable5);
+        double variable6 = 10.5d;
+        System.out.println(variable6);
+
+        //boleano
+        boolean variable7 = false;
+        System.out.println(variable7);
+        boolean variable8 = true;
+        System.out.println(variable8);
+
+        //texto
+        char variable9 = 'a';
+        String variable10="lll";
+        //los primirtivos con minusculasw, no puede ser nulos, los que no mayusculas, string es un obbjeto
+        // int, long, double, boolean, String.
+
+
+
+    }
+}
